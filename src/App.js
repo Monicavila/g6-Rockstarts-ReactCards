48a18c522694ed6ac1299eef98cab038d643b778
Mónica Ávila Moreno <moonita@Monicas-MacBook-Pro.local>
2021-06-22 23:53:18 -0500

    css improvement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const CreateCardCorner = (props) => {
+{/*const CreateCardCorner = (props) => {
   return (
     <div className="card-corner">
       <div>{props.number}</div>
       <div>{props.symbol}</div>
     </div>
   );
-};
+};*/}
 
 class CreateCardSymbols extends React.Component {
   constructor(props) {
@@ -20,15 +20,15 @@ class CreateCardSymbols extends React.Component {
 
   render() {
     return (
-      <div className="symbols">
-        {this.props.number === "A" ? (
-          <div className="symbol">{this.props.symbol}</div>
+      <div className="symbols" data={`${this.props.number}\n${this.props.symbol}`}>
+        {this.props.number === "A" ? ( 
+          <b>{this.props.symbol}</b>
         ) : ["J", "Q", "K"].includes(this.props.number) ? (
-          <div className="image"></div>
+          <div></div>
         ) : this.state.isNumber ? (
-          new Array(parseInt(this.props.number))
+          new Array(parseInt(this.props.number, 10))
             .fill(this.props.symbol)
-            .map((cardSymbol, index) => <div key={index}>{cardSymbol}</div>)
+            .map((cardSymbol, index) => <b key={index}>{cardSymbol}</b>)
         ) : null}
       </div>
     );
@@ -36,33 +36,33 @@ class CreateCardSymbols extends React.Component {
 }
 
 //class Card extends React.Component {
-  const Card = (props) => {
-    const [isFlipped, setFlip] = useState(props.flipped);
+  const Card = ({symbol, number, flipped}) => {
+    const [isFlipped, setFlip] = useState(flipped);
 
     return (
       <div
         className="card"
-        symbol={props.symbol}
-        number={props.number}
+        symbol={symbol}
+        number={number}
         className={["card", isFlipped ? "flipped" : ""].filter(Boolean).join(' ')}
         onClick={()=> {setFlip(!isFlipped)}}
       >
         <div className="container">
           <div className="front">
-            <CreateCardCorner
-              symbol={props.symbol}
-              number={props.number}
-            />
+            {/*<CreateCardCorner
+              symbol={symbol}
+              number={number}
+            />*/}
             <CreateCardSymbols
-              symbol={props.symbol}
-              number={props.number}
+              symbol={symbol}
+              number={number}
             />
-            <CreateCardCorner
-              symbol={props.symbol}
-              number={props.number}
-            />
+            {/*<CreateCardCorner
+              symbol={symbol}
+              number={number}
+            />*/}
           </div>
-          <div className="back"></div>
+          {/*<div className="back"></div>*/}
         </div>
       </div>
     );
@@ -116,8 +116,9 @@ class CreateCardSymbols extends React.Component {
 }*/}
 
 //class Deck extends React.Component
-const Deck = (props) => {
+const Deck = ({className, children, path, title, flipped}) => {
   const [cards, setCard] = useState([]);
+  
   {/*constructor(props) {
     super(props);
     this.state = {
@@ -140,32 +141,33 @@ const Deck = (props) => {
   useEffect(()=> {
     (async () => {
       const cards = await (
-        await fetch(`http://localhost:4001/${props.path}`)
+        await fetch(`http://localhost:4001/${path}`)
       ).json();
       setCard(cards);
     })();
-  }, [props.path]);
+  }, [path]);
 
   //render() {
     return (
-      <div>
+      <div className={['deck', className].filter(Boolean).join(' ')}>
         {cards.length === 0 ? (
           <div>Loading ... </div>
         ) : (
           <div>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="deck">
               {cards.map((card, index) => {
                 const number = card.slice(0, -1);
                 const symbol = card.slice(-1);
-                return <Card
-                    symbol={symbol}
+                const flippedCards = parseInt(flipped, 10) > index
+                return <Card {...{symbol, number, key : index, flipped : flippedCards}} />
+                    {/*symbol={symbol}
                     number={number}
                     key={index}
-                    flipped={parseInt(props.flipped) > index}
-                  />
+              flipped={flipCard}*/}
               })}
             </div>
+            {children}
           </div>
         )}
       </div>
@@ -173,13 +175,23 @@ const Deck = (props) => {
   //}
 }
 
+const FlipButton = ({flipped}) => {
+  const [isFlipped, setFlip] = useState(flipped);
+  return(
+    <button id="flip-cards" onClick={()=> {setFlip(!isFlipped)}}>Flip</button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Playing Texas</h1>
+        <Deck className="all" title="Deck" path="all/" flipped="52"/>
         <Deck title="Table" path="table/" flipped="2"/>
-        <Deck title="Hand" path="deck/2" flipped="2"/>
+        <Deck title="Hand" path="deck/2" flipped="2">
+          <FlipButton flipped={true}/>
+        </Deck>
       </header>
     </div>
   );
